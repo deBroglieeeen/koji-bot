0703c8f48ed1c10891ce9dd9e46c409c368778ab
deBroglieeeen <u825246d@ecs.osaka-u.ac.jp>
2020-05-03 15:30:44 +0900

    first-commit

---
+++ server.go
@@ -0,0 +1,52 @@
+package main
+
+ import (
+     "log"
+     "net/http"
+     "os"
+
+     "github.com/line/line-bot-sdk-go/linebot"
+     "github.com/line/line-bot-sdk-go/linebot/httphandler"
+ )
+
+ func main() {
+
+     // CHANNEL_SECRETとCHANNEL_TOKENはLine Developersから取得できる
+     handler, err := httphandler.New(
+         os.Getenv("CHANNEL_SECRET"),
+         os.Getenv("CHANNEL_TOKEN"),
+     )
+     if err != nil {
+         log.Fatal(err)
+     }
+
+     // Setup HTTP Server for receiving requests from LINE platform
+     handler.HandleEvents(func(events []*linebot.Event, r *http.Request) {
+         bot, err := handler.NewClient()
+         if err != nil {
+             log.Print(err)
+             return
+         }
+         for _, event := range events {
+             if event.Type == linebot.EventTypeMessage {
+                 switch message := event.Message.(type) {
+                 case *linebot.TextMessage:
+                     if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+                         log.Print(err)
+                     }
+                 }
+             }
+         }
+     })
+     http.Handle("/callback", handler)
+
+     //if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
+     //  log.Fatal(err)
+     //}
+
+     // HTTPではLine APIを利用できないため、HTTPSに変更する
+     // CERT_FILEとKEY_FILEはSSL証明書を発行した後に書き換える
+     if err := http.ListenAndServeTLS(":443", "CERT_FILE", "KEY_FILE", nil); err != nil {
+         logger.Fatal("ListenAndServe: ", err)
+     }
+ }
